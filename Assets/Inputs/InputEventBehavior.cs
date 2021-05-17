766b5071eaa2a08d284c19c78c6e790d10923376
EthanJones19 <69812858+EthanJones19@users.noreply.github.com>
2021-05-17 11:05:39 -0500

    Added player movement, player controls, and input event behavior

---
+++ Assets/Inputs/InputEventBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputEventBehavior : MonoBehaviour
+{
+    private PlayerMovementBehavior _move;
+    private PlayerMovementBehavior _skip;
+    private PlayerControls _playerInputs;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _playerInputs = new PlayerControls();
+    }
+
+    void Start()
+    {
+        _move = GetComponent<PlayerMovementBehavior>();
+        _playerInputs.Player.Movement.performed += context => _move.Movement((int)context.ReadValue<float>());
+        _skip = GetComponent<PlayerMovementBehavior>();
+        _playerInputs.Player.SkipMovement.performed += context => _skip.MovementSkipping((int)context.ReadValue<float>());
+    }
+
+    public void OnEnable()
+    {
+        _playerInputs.Enable();
+    }
+
+    public void OnDisable()
+    {
+        _playerInputs.Disable();
+    }
+}
