164a398737906a205f8320ed5936a10e3061b8e4
EthanJones19 <Ethanjones800@yahoo.com>
2021-05-17 17:00:44 -0500

    Added serving behavior

---
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("KillPlane"))
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+    }
+}
