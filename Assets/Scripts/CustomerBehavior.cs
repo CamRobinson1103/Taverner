7de38084b375f7e8aa2fc636ee9342be5fac4fa6
CamRobinson <robinsoncameron1103@gmail.com>
2021-05-21 11:52:39 -0500

    Added Collision with mug and customer; Working on boolean and timer

---
+++ Assets/Scripts/CustomerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerBehavior : MonoBehaviour
+{
+    bool isDrinking = false;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Mug")
+        {
+          col.gameObject.GetComponent<SlideBackBehavior>();
+            isDrinking = true;
+        }
+
+        if (col.gameObject.tag == "Despawner")
+        {
+            Destroy(gameObject);
+        }
+
+        if (isDrinking == true);
+        {
+
+        }
+            
+    }
+}
