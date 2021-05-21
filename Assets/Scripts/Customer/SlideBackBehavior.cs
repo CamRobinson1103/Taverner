bea6b4e98226fb32c5ef38a8f3091a0e6a5fdf2c
Cameron Robinson <robinsoncameron1103@@gmail.com>
2021-05-20 20:51:47 -0500

    Added SlideBack Behavior; working on Despawn poinr

---
+++ Assets/Scripts/Customer/SlideBackBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideBackBehavior : MonoBehaviour
+{
+    public float SlideBack = 5;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject)
+        {
+            transform.position -= new Vector3(0, 0, SlideBack) * Time.deltaTime;
+        }
+    }
+}
