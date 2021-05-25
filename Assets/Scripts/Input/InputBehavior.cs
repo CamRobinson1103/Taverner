d0aad8aae9c9e55cd1a672257c11cc765e2b0f8b
Cameron Robinson <robinsoncameron1103@@gmail.com>
2021-05-25 14:57:21 -0500

    Revert "Recreated Set"
    This reverts commit 707f5e57c12351ca3bb42cb039365b845871b37e.

--- Assets/Scripts/Input/InputBehavior.cs
+++ Assets/Scripts/Input/InputBehavior.cs
@@ -8,10 +8,15 @@ public class InputBehavior : MonoBehaviour
     //Reference for the shooting, Serve behavior.
     private ServeBehavior _serve;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     //Reference for the movement, Player movement.
     //private PlayerMovementBehavior _movement;
 >>>>>>> Cam'sBranch
+=======
+    //Reference for the movement, Player movement.
+    //private PlayerMovementBehavior _movement;
+>>>>>>> parent of 707f5e5... Recreated Set
     //Reference for the player controls.
     private PlayerControls _playerControls;
 
@@ -26,10 +31,15 @@ public class InputBehavior : MonoBehaviour
         _serve = GetComponent<ServeBehavior>();
         _playerControls.Player.Shoot.performed += context => _serve.Shoot();
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         //_movement = GetComponent<PlayerMovementBehavior>();
         //_playerControls.Player.Movement.performed += context => _movement.Move((int)context.ReadValue<float>());
 >>>>>>> Cam'sBranch
+=======
+        //_movement = GetComponent<PlayerMovementBehavior>();
+        //_playerControls.Player.Movement.performed += context => _movement.Move((int)context.ReadValue<float>());
+>>>>>>> parent of 707f5e5... Recreated Set
     }
 
     public void OnEnable()
