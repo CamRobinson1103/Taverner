e9a27ed66e52b7e615300b15c45ea89b58ec7b14
Benjamin Brownlie <benjibspam@gmail.com>
2021-05-25 14:22:51 -0500

    Revert "Merge remote-tracking branch 'origin/Ben's-branch' into Ben's-branch"
    This reverts commit 3b8b05033b0f6238cd10aeefe08f512d7995fb4f.

---
+++ Assets/Scripts/Movement B/EnemyMovementBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementBehavior : MonoBehaviour
+{
+    public float speed;
+    public new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+    }
+}
