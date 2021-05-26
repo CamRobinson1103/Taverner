0d33d70163c5e606b99cf86edcb9c4e4d8e6fc5b
CamRobinson <robinsoncameron1103@gmail.com>
2021-05-26 15:04:32 -0500

    Added Lerp; still adjusting

--- Assets/Scripts/Customer/CustomerMovementBehavior.cs
+++ Assets/Scripts/Customer/CustomerMovementBehavior.cs
@@ -5,6 +5,30 @@ using UnityEngine;
 public class CustomerMovementBehavior : MonoBehaviour
 {
     public float HSpeed = 2;
+    float lerpDuration = 3;
+    float startValue = 0;
+    float endValue = 10;
+    float valueToLerp;
+
+    void Start()
+    {
+        StartCoroutine(Lerp());
+    }
+
+    IEnumerator Lerp()
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < lerpDuration)
+        {
+            valueToLerp = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        valueToLerp = endValue;
+    }
 
     // Update is called once per frame
     void Update()
@@ -12,4 +36,5 @@ public class CustomerMovementBehavior : MonoBehaviour
         transform.position -= new Vector3(0, 0, HSpeed) * Time.deltaTime;
 
     }
+    
 }
