c0bed5d3c292ec7f40524ef954ba7d454b44b63e
EthanJones19 <Ethanjones800@yahoo.com>
2021-05-26 16:40:41 -0500

    Added a spawner behavior and temporarily movement for object

---
+++ Assets/Scripts/Ethan's Folder/Gameplay/SpawnerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehavior : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject _objectSpawn;
+    [SerializeField]
+    private bool _canspawntime;
+    [SerializeField]
+    private float _nextspawnTime = 5;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+
+    public IEnumerator Spawn()
+    {
+        while (_canspawntime)
+        {
+            GameObject gameobject = Instantiate(_objectSpawn, transform.position, new Quaternion());
+            yield return new WaitForSeconds(_nextspawnTime);
+        }
+    }
+}
