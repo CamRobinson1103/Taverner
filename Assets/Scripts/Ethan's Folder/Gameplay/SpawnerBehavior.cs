e70d20c8127b2c816276b0dc1a816abba1777154
EthanJones19 <69812858+EthanJones19@users.noreply.github.com>
2021-05-28 11:58:37 -0500

    Added a random number range

--- Assets/Scripts/Ethan's Folder/Gameplay/SpawnerBehavior.cs
+++ Assets/Scripts/Ethan's Folder/Gameplay/SpawnerBehavior.cs
@@ -7,9 +7,14 @@ public class SpawnerBehavior : MonoBehaviour
     [SerializeField]
     public GameObject _objectSpawn;
     [SerializeField]
+    public GameObject _objectSpawn2;
+    [SerializeField]
     private bool _canspawntime;
     [SerializeField]
     private float _nextspawnTime = 5;
+    [SerializeField]
+    private float _randomTime;
+
 
     private void Start()
     {
@@ -24,5 +29,18 @@ public class SpawnerBehavior : MonoBehaviour
             GameObject gameobject = Instantiate(_objectSpawn, transform.position, new Quaternion());
             yield return new WaitForSeconds(_nextspawnTime);
         }
+
+        while (_canspawntime)
+        {
+            GameObject gameobject = Instantiate(_objectSpawn2, transform.position, new Quaternion());
+            yield return new WaitForSeconds(_randomTime);
+        }
     }
+
+
+    public void RandomTime()
+    {
+        _randomTime = Random.Range(1, 5);
+    }
+
 }
