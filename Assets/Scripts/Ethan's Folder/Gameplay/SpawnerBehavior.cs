4b047afccb59000312aef6a065f8f785158fa149
EthanJones19 <69812858+EthanJones19@users.noreply.github.com>
2021-06-02 16:26:37 -0500

    Added another spawner point and removed spawn 2 stuff in Spawner behavior

--- Assets/Scripts/Ethan's Folder/Gameplay/SpawnerBehavior.cs
+++ Assets/Scripts/Ethan's Folder/Gameplay/SpawnerBehavior.cs
@@ -7,17 +7,9 @@ public class SpawnerBehavior : MonoBehaviour
     [SerializeField]
     private GameObject _objectSpawn;
     [SerializeField]
-    private GameObject _objectSpawn2;
-    [SerializeField]
     private float _nextspawnTime;
     [SerializeField]
-    private float _nextspawnTime2;
-    [SerializeField]
     private int _spawnCount;
-    [SerializeField]
-    private int _spawnCount2;
-
-
 
     private void Start()
     {
@@ -32,13 +24,6 @@ public class SpawnerBehavior : MonoBehaviour
             GameObject gameobject = Instantiate(_objectSpawn, transform.position, new Quaternion());
             yield return new WaitForSeconds(_nextspawnTime);
         }
-
-        for (int count = _spawnCount2; count > 0; --count)
-        {
-            GameObject gameobject2 = Instantiate(_objectSpawn2, transform.position, new Quaternion());
-            yield return new WaitForSeconds(_nextspawnTime2);
-        }
-
     }
 
 
