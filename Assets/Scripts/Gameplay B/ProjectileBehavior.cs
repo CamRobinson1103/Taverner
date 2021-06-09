c0ce2f484482ac8a0d46fbc14588fd178bc1e459
Ben Brownlie <benjibspam@gmail.com>
2021-06-09 16:52:52 -0500

    Added comments
    Added comments to ProjectileBehavior, ScoreDropBehavior, and ServeBehavior.

--- Assets/Scripts/Gameplay B/ProjectileBehavior.cs
+++ Assets/Scripts/Gameplay B/ProjectileBehavior.cs
@@ -6,9 +6,10 @@ public class ProjectileBehavior : MonoBehaviour
 {
     //Reference to the projectile's rigidbody
     public Rigidbody rigidbody;
-    //
+    //The projectiles velocity
     private Vector3 _velocity;
     [SerializeField]
+    //Reference to the scoreboard
     private GameManagerBehavior score;
 
     public Vector3 Velocity
@@ -26,21 +27,27 @@ public class ProjectileBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Calls GetComponent for the projectile's rigidbody
         rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Checks if the collider is the KillPlane
         if (other.CompareTag("KillPlane"))
+            //Destroys the projectile
             Destroy(gameObject);
 
+        //Checks if the collider is a customer(enemy)
         else if (other.CompareTag("Enemy"))
+            //Adds to the scoreboard
             score.AddScore(5);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Moves the projectile
         rigidbody.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
 }
--- Assets/Scripts/Gameplay B/ScoreDropBehavior.cs
+++ Assets/Scripts/Gameplay B/ScoreDropBehavior.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ScoreDropBehavior : MonoBehaviour
 {
+    //Reference to the object that will be dropped
     public GameObject boost;
+    //The highest random number
     public int maxNumber;
+    //The lowest random number
     public int leastNumber;
 
     // Start is called before the first frame update
@@ -16,13 +19,18 @@ public class ScoreDropBehavior : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //Generates a random number from the range of passed in numbers
         int rand = Random.Range(leastNumber, maxNumber);
 
+        //Checks if the collider is a projectile ("Mug"
         if (other.CompareTag("Mug"))
         {
+            //Checks if the random number is 2
             if (rand == 2)
+                //If so, spawns a boost object
                 Instantiate(boost.gameObject, gameObject.transform.position, gameObject.transform.rotation);
 
+            //Destroys the projectile
             Destroy(other.gameObject);
         }
     }
