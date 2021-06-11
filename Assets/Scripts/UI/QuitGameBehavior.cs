b4683d584c4d9f0a9bc2b091bbd1320024abf26a
EthanJones19 <69812858+EthanJones19@users.noreply.github.com>
2021-06-11 09:24:45 -0500

    Revert "Added Game Over screen and added restart behavior. Made comments to StartMenu, RestartGame, and QuitGame behavior."
    This reverts commit 908ac7eb2c356b50bc4bc09dc3e6b9fb8d7aa942.

--- Assets/Scripts/UI/QuitGameBehavior.cs
+++ Assets/Scripts/UI/QuitGameBehavior.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class QuitGameBehavior : MonoBehaviour
 {
-    //Quits the application
     public void QuitGame()
     {
         Application.Quit();
--- Assets/Scripts/UI/StartMenuBehavior.cs
+++ Assets/Scripts/UI/StartMenuBehavior.cs
@@ -6,17 +6,19 @@ public class StartMenuBehavior : MonoBehaviour
 {
     public GameObject gamePause;
 
-    //Game will unpause and play
+
     public void GamePlays()
     {
+
         gamePause.SetActive(false);
         Time.timeScale = 1f;
+
     }
 
-    //Game is pause in the background
     public void Start()
     {
         gamePause.SetActive(true);
         Time.timeScale = 0f;
+
     }
 }
