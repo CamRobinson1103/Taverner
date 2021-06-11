ce54e100d32a00a32b6741eb2af69e508a62ac1c
Ben Brownlie <benjibspam@gmail.com>
2021-06-11 08:51:58 -0500

    Player Lives
    Added a counter for player lives to the screen, set to 3 by default.
    
    If the player fires a projectile and it collides with the killplane, a life will be removed from the counter.

---
+++ Assets/Scripts/Gameplay B/KillPlaneBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlaneBehavior : MonoBehaviour
+{
+    //Reference to the game manager
+    public GameManagerBehavior playerLives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Mug"))
+        {
+            playerLives.RemoveLife(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
