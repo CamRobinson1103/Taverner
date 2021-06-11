74967bf8c3dbe16cd602d9cadbadf9f1f136e018
CamRobinson <robinsoncameron1103@gmail.com>
2021-06-11 11:41:42 -0500

    Adding comments

--- Assets/Scripts/Customer/SlideBackBehavior.cs
+++ Assets/Scripts/Customer/SlideBackBehavior.cs
@@ -22,8 +22,7 @@ public class SlideBackBehavior : MonoBehaviour
     {
         transform.position = Vector3.Lerp(transform.position, newPosition, move * Time.deltaTime);
     }
-    //I thought I had something. Japanese sounds so good to me. I'm... nvm I was faded in that last song
-    // TALK TO THESE NIGGAS JOHNATHAN
+    
     private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.CompareTag ("Mug"))
