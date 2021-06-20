f1228dd7db8c5f752e89dae18d27e118d2205702
Cameron Robinson <robinsoncameron1103@@gmail.com>
2021-06-20 18:46:14 -0500

    Adjusting movement after collision

--- Assets/Scripts/Customer/TimerBehavior.cs
+++ Assets/Scripts/Customer/TimerBehavior.cs
@@ -4,47 +4,5 @@ using UnityEngine;
 
 public class TimerBehavior : MonoBehaviour
 {
-    public float timeRemaining = 3;
-    public bool timerIsRunning = false;
-    public float HSpeed = 2;
-
-    private void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        if (timerIsRunning)
-        {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                timeRemaining = 0;
-                timerIsRunning = false;
-            }
-            if (timeRemaining == 0)
-            {
-                Debug.Log("Timer works");
-            }
-
-        }
-     
-    }
-
-    private void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.CompareTag("Mug"))
-        {
-            timerIsRunning = true;
-        }
-
-        if (timeRemaining == 0)
-        {
-            transform.position -= new Vector3(0, 0, HSpeed) * Time.deltaTime;
-        }
-    }
+    
 }
