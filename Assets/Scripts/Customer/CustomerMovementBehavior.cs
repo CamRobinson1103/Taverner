e5b2fd28216e7ff96911f3f8c6e29521bb75cd45
CamRobinson <robinsoncameron1103@gmail.com>
2021-06-21 15:02:36 -0500

    Made TankBehavior for tank enemy

--- Assets/Scripts/Customer/CustomerMovementBehavior.cs
+++ Assets/Scripts/Customer/CustomerMovementBehavior.cs
@@ -5,44 +5,16 @@ using UnityEngine;
 public class CustomerMovementBehavior : MonoBehaviour
 {
     public float HSpeed = 2;
-    public float timeRemaining = 3;
-    public bool timerIsRunning = false;
-
     void Update()
     {
         Move();
-
-        if (timerIsRunning)
-        {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                Debug.Log("Timer works");
-                timeRemaining = 0;
-                timerIsRunning = false;
-            }
-        }
-
-        if(timeRemaining == 0)
-        {
-            Move();
-        }
     }
 
-     void Move()
+    void Move()
      {
         transform.position -= new Vector3(0, 0, HSpeed) * Time.deltaTime;
      }        
 
-    private void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.CompareTag("Mug"))
-        {
-            timerIsRunning = true;
-        }
-    }
+    
 }
 
