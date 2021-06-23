840da082a958d7d148397c0230931d9f58fa20e6
Ben Brownlie <benjibspam@gmail.com>
2021-06-23 16:34:58 -0500

    Idle loop and PlayerAnimationBehavior
    Added loop to the idle animation for the bartender
    
    began on PlayerAnimationBehavior script

---
+++ Assets/Scripts/Gameplay B/PlayerAnimationBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationBehavior : MonoBehaviour
+{
+
+    public ServeBehavior serve;
+    // Start is called before the first frame update
+    void Start()
+    {
+        serve = GetComponent<ServeBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
